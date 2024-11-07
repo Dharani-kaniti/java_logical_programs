aebe7204e6e8ad24e7d54c1ec549ef233e7d7234
Dharani-kaniti <dharanikaniti82@gmail.com>
2024-11-07 21:50:21 +0530

    Add files via upload

---
+++ Day 2/Num.java
@@ -0,0 +1,16 @@
+class Num
+{
+public static void main(String args[])
+{
+int a=5;
+switch(a%2)
+{
+case 0:
+System.out.println("even");
+break;
+case 1:
+System.out.println("odd");
+break;
+}
+}
+}
