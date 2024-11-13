a881b3969ae9a14e82028f4743f6452a16582669
Dharani-kaniti <dharanikaniti82@gmail.com>
2024-11-13 19:38:49 +0530

    Add files via upload

--- day 3/Reverse.java
+++ day 3/Reverse.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-public class Reverse
-{
-public static void main(String args[])
-{
-Scanner s=new Scanner(System.in);
-int i,n;
-System.out.println("enter n natural numbers:");
-n=s.nextInt();
-i=10;
-while(i>=n)
-{
-System.out.println(i);
-i--;
-}
-}
-}
 
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int reverse = 0;
+
+                while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+                System.out.println("Reversed number: " + reverse);
+    }
+}
