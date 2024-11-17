328d294252e9fe50f404d669c0a22d5aa39420bb
Dharani-kaniti <dharanikaniti82@gmail.com>
2024-11-17 18:49:09 +0530

    Add files via upload

---
+++ day 5/Concatenation.java
@@ -0,0 +1,18 @@
+public class Concatenation
+{
+public static void main(String args[])
+{
+String st="dharani";
+String st1="Uma";
+System.out.println(st.length());
+System.out.println(st1.length());
+if(st.equals(st1))
+{
+System.out.println("both are equal");
+}
+else
+{
+System.out.println("not equal");
+}
+}
+}
